c4425400dd7237b6db50579e012883c9d6844cf7
ISHIKA <500062316@DDN.UPES.AC.IN>
2018-06-07 10:48:02 +0530

    commit my prog2

---
+++ temp.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class temp1
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+int c,f;
+System.out.println("enter the temprature in fahrenhite");
+f =sc.nextInt();
+c =(5/9)*(f-32);
+System.out.println("the temprature in degree celcius is "+c);
+}
+}
