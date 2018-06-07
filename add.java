9c245b19b0863cb0c50afc4cda7419930f34f6a3
ISHIKA <500062316@DDN.UPES.AC.IN>
2018-06-07 10:19:37 +0530

    commit prog1

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class add1
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+int a,b,c;
+System.out.println("enter first number");
+a = sc.nextInt();
+System.out.println("enter second number");
+b = sc.nextInt();
+c = a+b;
+System.out.println("the sum of two numbers is"+c);
+}
+}
